6f1e97a552ce12b2663f7ce11cfdbcaab5424a13
anavalca <avc92j@gmail.com>
2022-09-26 21:17:26 +0200

    background waves

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,8 +1,14 @@
+import img from '../images/waves.svg'
+import img2 from '../images/waves2.svg'
 
 export const InfoSection = () => {
 
   return (
-    <div className='infoBackground' id="home">
+    <div className='infoSection'>
+      <img alt="" src={img}></img>
+      <div className='infoBackground' id="home">
+      </div>
+      <img alt="" src={img2}></img>
     </div>
   );
 }
