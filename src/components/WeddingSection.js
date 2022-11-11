bbb60f7132e4df4517bb5f78231e2a4f3374eac4
anavalca <avc92j@gmail.com>
2022-11-11 11:57:57 +0100

    modify pre-wedding text

--- src/components/WeddingSection.js
+++ src/components/WeddingSection.js
@@ -11,7 +11,7 @@ import Modal from 'react-modal'
 import { Map } from './Map'
 
 export const WeddingSection = () => {
-  
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleModal = () => {
@@ -19,15 +19,15 @@ export const WeddingSection = () => {
   }
 
   return (
-    <div className='text-white bg-sea-blue' id='wedding'>
-      <img alt='' src={wavesImgMobile} className='lg:hidden'/>
-      <img alt='' src={wavesImg} className='hidden lg:block'/>
-      <div className='p-8 pt-20 lg:pt-24 lg:pb-4 lg:m-auto lg:w-800 flex flex-col lg:flex-row justify-center items-start' id='wedding' data-aos='fade-up' data-aos-duration='2000' >
+    <div className='text-white bg-sea-blue flex flex-col items-center' id='wedding'>
+      <img alt='' src={wavesImgMobile} className='lg:hidden' />
+      <img alt='' src={wavesImg} className='hidden lg:block' />
+      <div className='p-8 pt-20 lg:pt-24 lg:pb-4 lg:w-800 flex flex-col lg:flex-row justify-center items-start'  data-aos='fade-up' data-aos-duration='2000' >
         <div className='w-full lg:w-1/2 lg:pr-7 pb-10 lg:pb-0 text-center lg:text-left lg:text-right'>
           <h2 className='text-4xl lg:text-6xl pb-5 lg:pb-2 font-bold lg:pl-0'>¡Vamos de boda!</h2>
           <p className='font-light pb-3'>No podemos celebrar este día tan especial sin ti, por eso hemos montado una super fiesta en la playa a la que estás invitado.</p>
           <p className='font-light'>¡No te preocupes por nada! Iremos en autobús hasta el restaurante donde se celebrará todo el evento. ¿Te apuntas?</p>
-          
+
         </div>
         <div className='w-full lg:w-1/2 lg:pl-7 pt-6 lg:pt-0 lg:border-l-2 flex flex-wrap lg:flex-col font-medium text-center lg:text-left'>
           <div className='w-1/2 lg:w-auto flex flex-col lg:flex-row items-center pb-11 lg:pb-7'>
@@ -51,6 +51,11 @@ export const WeddingSection = () => {
             <p className='lg:pl-4 text-normal'>Horario y lugar de recogida del autobús por determinar</p>
           </div>
         </div>
+      </div>
+      <div className='m-8 w-auto lg:w-800 mt-5 lg:mt-12 p-5 lg:mb-5 border-2' data-aos='fade-up' >
+        <h4 className='text-3xl font-bold lg:pl-0'>Preboda</h4>
+        <p className='font-light'>El viernes, 2 de junio, os esperamos en la playa de Regla para disfrutar de unas cervezas fresquitas juntos. ¡Nada de dresscode ni tacones! Previamente se hará una ofrenda a la Virgen de Regla en el Santuario.</p>
+        <p className='font-light'></p>
       </div>
       {/* POP UP */}
       <Modal
